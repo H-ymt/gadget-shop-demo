ac3d1853f0d99ed1dcbd55609ff8f7d3014781a6
Handai-Yamato <y,handai@crmlab.jp>
2022-12-28 15:29:29 +0900

    add SSG

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import React from "react";
 import { MediaQuery } from "../../components/mediaquery";
 import { css } from "@emotion/react";
 import { motion } from "framer-motion";
@@ -32,81 +33,119 @@ const stagger = {
   },
 };
 
-const Product = (props) => (
-  <Layout>
-    <motion.div exit={{ opacity: 0 }} variants={fadeInUp} initial="hidden" animate="visible">
-      <div>
-        <div css={container}>
-          <div css={img}>
-            <motion.div initial={{ x: 200, opacity: 0 }} animate={{ x: 0, opacity: 1 }}>
-              <Image
-                alt=""
-                key={props.product.image}
-                src={props.product.image}
-                transition={{ delay: 0.3 }}
-                width={320}
-                height={320}
-              />
-            </motion.div>
-          </div>
-          <div css={details}>
-            <motion.div variants={stagger} initial="init" animate="show" css={detailsInner}>
-              <Link href="/">
-                <motion.div variants={fadeInUp} initial="hidden" animate="visible">
-                  <div>Back to products</div>
+const Product = ({ product }) => {
+  return (
+    <Layout>
+      <motion.div exit={{ opacity: 0 }} variants={fadeInUp} initial="hidden" animate="visible">
+        <div>
+          <div css={container}>
+            <div css={img}>
+              <motion.div initial={{ x: 200, opacity: 0 }} animate={{ x: 0, opacity: 1 }}>
+                <Image
+                  alt=""
+                  key={product.image}
+                  src={product.image}
+                  transition={{ delay: 0.3 }}
+                  width={320}
+                  height={320}
+                />
+              </motion.div>
+            </div>
+            <div css={details}>
+              <motion.div variants={stagger} initial="init" animate="show" css={detailsInner}>
+                <Link href="/">
+                  <motion.div variants={fadeInUp} initial="hidden" animate="visible">
+                    <div>Back to products</div>
+                  </motion.div>
+                </Link>
+
+                <motion.h1
+                  variants={fadeInUp}
+                  initial="hidden"
+                  animate="visible"
+                  css={{ marginTop: "16px", fontSize: "18px", fontWeight: "700" }}
+                >
+                  {product.name}
+                </motion.h1>
+                <motion.p
+                  variants={fadeInUp}
+                  initial="hidden"
+                  animate="visible"
+                  css={{ marginTop: "16px" }}
+                >
+                  {product.details}
+                </motion.p>
+
+                <motion.div variants={fadeInUp} initial="hidden" animate="visible" css={qtyPrice}>
+                  <div css={qty}>
+                    <div css={[qtyButton, minus]}>-</div>
+                    <div>1</div>
+                    <div css={[qtyButton, add]}>+</div>
+                  </div>
+                  <span>{product.price}</span>
                 </motion.div>
-              </Link>
-
-              <motion.h1
-                variants={fadeInUp}
-                initial="hidden"
-                animate="visible"
-                css={{ marginTop: "16px", fontSize: "18px", fontWeight: "700" }}
-              >
-                {props.product.name}
-              </motion.h1>
-              <motion.p
-                variants={fadeInUp}
-                initial="hidden"
-                animate="visible"
-                css={{ marginTop: "16px" }}
-              >
-                {props.product.details}
-              </motion.p>
-
-              <motion.div variants={fadeInUp} initial="hidden" animate="visible" css={qtyPrice}>
-                <div css={qty}>
-                  <div css={[qtyButton, minus]}>-</div>
-                  <div>1</div>
-                  <div css={[qtyButton, add]}>+</div>
-                </div>
-                <span>{props.product.price}</span>
+                <motion.div
+                  variants={fadeInUp}
+                  initial="hidden"
+                  animate="visible"
+                  css={{ display: "flex", marginTop: "16px" }}
+                >
+                  <button css={[button, addCart]}>Add</button>
+                  <button css={[button, subscribe]}>Subscribe</button>
+                </motion.div>
               </motion.div>
-              <motion.div
-                variants={fadeInUp}
-                initial="hidden"
-                animate="visible"
-                css={{ display: "flex", marginTop: "16px" }}
-              >
-                <button css={[button, addCart]}>Add</button>
-                <button css={[button, subscribe]}>Subscribe</button>
-              </motion.div>
-            </motion.div>
+            </div>
           </div>
         </div>
-      </div>
-    </motion.div>
-  </Layout>
-);
-
-Product.getInitialProps = async function (context) {
-  const { id } = context.query;
-  const res = await fetch(
-    `http://my-json-server.typicode.com/Handai-Yamato/gadget-shop-db/products/${id}`
+      </motion.div>
+    </Layout>
   );
-  const product = await res.json();
-  return { product };
 };
+
+export const getStaticPaths = async () => {
+  // 外部APIを呼び出しデータを取得
+  const res = await fetch(
+    "http://my-json-server.typicode.com/Handai-Yamato/gadget-shop-db/products"
+  );
+  const products = await res.json();
+
+  // 事前ビルドしたいパスを指定
+  const paths = products.map((product) => ({
+    params: {
+      // ファイル名と合わせる
+      id: product.id.toString(),
+    },
+  }));
+
+  // paths：事前ビルドするパス対象を指定するパラメータ
+  // fallback：事前ビルドしたパス以外にアクセスしたときのパラメータ true:カスタム404Pageを表示 false:404pageを表示
+  return { paths, fallback: false };
+};
+
+// paramsには上記pathsで指定した値が入る（1つずつ）
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(
+    `http://my-json-server.typicode.com/Handai-Yamato/gadget-shop-db/products/${params.id}`
+  );
+
+  const product = await res.json();
+
+  // ページコンポーネントにproductとして渡す
+  return {
+    props: {
+      product,
+    },
+  };
+};
+
+// Product.getInitialproducts = async function (context) {
+//   const { id } = context.query;
+//   const res = await fetch(
+//     `http://my-json-server.typicode.com/Handai-Yamato/gadget-shop-db/products/${id}`
+//   );
+//   const product = await res.json();
+//   return { product };
+// };
 
 export default Product;
 
